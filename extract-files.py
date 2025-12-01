3e5ac79cab05e470156b46df96c738552d42f580
realzarakinyu <onlyzarakinyu@gmail.com>
2025-12-01 16:43:38 +0530

    sm4635-common: Build more audio blobs from source

--- extract-files.py
+++ extract-files.py
@@ -4,18 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from extract_utils.file import File
+import extract_utils.tools
 from extract_utils.fixups_blob import (
-    BlobFixupCtx,
     blob_fixup,
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove_arch_suffix,
-    lib_fixup_vendorcompat,
+    lib_fixup_remove,
+    lib_fixups,
     lib_fixups_user_type,
-    libs_clang_rt_ubsan,
-    libs_proto_3_9_1,
 )
 from extract_utils.main import (
     ExtractUtils,
@@ -24,22 +21,98 @@ from extract_utils.main import (
 
 namespace_imports = [
     'device/xiaomi/sm4635-common',
+    'hardware/qcom-caf/wlan',
     'hardware/qcom-caf/sm8650',
     'hardware/xiaomi',
+    'vendor/qcom/opensource/display',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/dataservices',
 ]
 
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}-{partition}' if partition == 'vendor' else None
 
-
 lib_fixups: lib_fixups_user_type = {
-    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
-    libs_proto_3_9_1: lib_fixup_vendorcompat,
+    **lib_fixups,
     (
+        'vendor.qti.diaghal@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+        'vendor.qti.imsrtpservice@3.1',
+        'vendor.qti.ImsRtpService-V1-ndk'
     ): lib_fixup_vendor_suffix,
+    (
+        'android.hardware.graphics.allocator-V1-ndk',
+        'android.hardware.graphics.composer3-V2-ndk',
+        'audio.primary.pitti',
+        'libagmclient',
+        'libagmmixer',
+        'libar-acdb',
+        'libats',
+        'liblx-osal',
+        'liblx-ar_util',
+        'libar-gpr',
+        'libar-gsl',
+        'libpalclient',
+        'libwpa_client',
+        'vendor.qti.hardware.display.composer3-V1-ndk',
+        'vendor.qti.hardware.AGMIPC@1.0-impl',
+        'vendor.qti.hardware.pal@1.0-impl',
+    ): lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/sensors/hals.conf': blob_fixup()
+        .add_line_if_missing('sensors.xiaomi.v2.so'),
+    (
+        'system_ext/lib64/libcameraopt.so',
+        'vendor/lib64/hw/camera.qcom.so'
+    ): blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
+    (
+        'vendor/lib64/libanc_single_rt_bokeh.so',
+        'vendor/lib64/libTrueSight.so',
+        'vendor/lib64/libMiVideoFilter.so',
+    ): blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_isSupported')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_lockPlanes')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+    (
+        'vendor/etc/media_codecs_pitti.xml', 
+        'vendor/etc/media_codecs_pitti_32go.xml'
+    ): blob_fixup()
+        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', ''),
+    'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
+        .add_needed('libhidlbase_shim.so')
+        .add_needed('libbinder_shim.so'),
+    'vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy': blob_fixup()
+        .add_line_if_missing('setsockopt: 1'),
+    (
+        'vendor/lib64/libqcc_sdk.so', 
+        'vendor/lib64/libqms_client.so',
+        'vendor/bin/qcc-vendor',
+        'vendor/bin/xtra-daemon',
+        'vendor/bin/qms',
+        'vendor/bin/cnd',
+        'vendor/lib64/libcne.so'
+    ): blob_fixup()
+        .add_needed('libbinder_shim.so'),
+
+    ('vendor/bin/hw/vendor.qti.media.c2@1.0-service',
+     'vendor/bin/hw/vendor.qti.media.c2audio@1.0-service'
+     ): blob_fixup()
+        .add_needed('libcodec2_hidl_shim.so'),
+
+    'vendor/lib64/libqcodec2_core.so': blob_fixup()
+        .add_needed('libcodec2_shim.so'),
+
+    ('vendor/bin/hw/vendor.qti.media.c2@1.0-service',
+     'vendor/bin/hw/vendor.qti.media.c2audio@1.0-service'): blob_fixup()
+        .add_needed('libcodec2_hidl_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
@@ -48,6 +121,7 @@ module = ExtractUtilsModule(
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
+    check_elf=True,
 )
 
 if __name__ == '__main__':
