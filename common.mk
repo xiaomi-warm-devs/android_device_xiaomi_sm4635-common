05ba5a5bb0809c394a81589f477b1e4782690c96
realzarakinyu <onlyzarakinyu@gmail.com>
2025-12-01 16:35:33 +0530

    sm4635-common: Include virtual AB, storage, Dalvik, ramdisk, virtualization, and QCOM common

--- common.mk
+++ common.mk
@@ -7,6 +7,21 @@
 # Call the proprietary setup.
 $(call inherit-product, vendor/xiaomi/sm4635-common/sm4635-common-vendor.mk)
 
+# Enable virtual AB with vendor ramdisk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Enforce generic ramdisk allow list
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
+# Virtualization service
+$(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
+
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
